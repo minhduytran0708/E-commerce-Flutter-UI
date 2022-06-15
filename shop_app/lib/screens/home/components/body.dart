68aab4815b899ac78805f170cf7ad0dfee3ec116
SENDO\DuyTM <duytm@sendo.vn>
2022-06-15 09:08:42 +0700

    home

--- shop_app/lib/screens/home/components/body.dart
+++ shop_app/lib/screens/home/components/body.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/size_config.dart';
-
 import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
-import 'section_title.dart';
+import 'popular_products.dart';
+import 'special_offers.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -19,59 +19,10 @@ class Body extends StatelessWidget {
         SizedBox(height: getProportionateScreenWidth(30)),
         Categories(),
         SizedBox(height: getProportionateScreenWidth(30)),
-        SectionTitle(
-          text: "Special for you",
-          press: () {},
-        ),
-        SizedBox(
-          width: getProportionateScreenWidth(242),
-          height: getProportionateScreenWidth(100),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Stack(
-              children: [
-                Image.asset(
-                  "assets/images/Image Banner 2.png",
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color(0xFF343434).withOpacity(0.4),
-                        Color(0xFF343434).withOpacity(0.15),
-                      ]
-                    )
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(15),
-                    vertical: getProportionateScreenWidth(10),
-                  ),
-                  child: Text.rich(
-                    TextSpan(
-                      style: TextStyle(color: Colors.white),
-                      children: [
-                        TextSpan(
-                          text: "Smartphone\n",
-                          style: TextStyle(
-                            fontSize: getProportionateScreenWidth(18),
-                            fontWeight: FontWeight.bold,
-                          )
-                        ),
-                        TextSpan(
-                          text: "18 Brands",
-                        ),
-                      ]
-                    )
-                  ),
-                )
-            ]),
-          ),
-        )
+        SpecialOffers(),
+        SizedBox(height: getProportionateScreenWidth(30)),
+        PopularProducts(),
+        SizedBox(height: getProportionateScreenWidth(30)),
       ])),
     );
   }
