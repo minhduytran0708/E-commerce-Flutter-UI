924816161c256a2c356ca4df5116beb3a510e573
SENDO\DuyTM <duytm@sendo.vn>
2022-06-20 07:55:32 +0700

    up

--- shop_app/lib/screens/details/components/body.dart
+++ shop_app/lib/screens/details/components/body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/constants.dart';
-import 'package:shop_app/size_config.dart';
 
 import '../../../models/Product.dart';
+import 'color_dots.dart';
 import 'product_description.dart';
 import 'product_images.dart';
 import 'top_rounded_container.dart';
@@ -31,69 +30,6 @@ class Body extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class ColorDots extends StatelessWidget {
-  const ColorDots({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
-
-  final Product product;
-
-  @override
-  Widget build(BuildContext context) {
-    int selectedColor = 3;
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: getProportionateScreenWidth(20),
-      ),
-      child: Row(
-        children: [
-          ...List.generate(
-            product.colors.length,
-            (index) => ColorDot(
-              color: product.colors[index],
-              isSelected: selectedColor == index,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  const ColorDot({
-    Key? key,
-    required this.color,
-    this.isSelected = false,
-  }) : super(key: key);
-
-  final Color color;
-  final bool isSelected;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 2),
-      padding: EdgeInsets.all(8),
-      height: getProportionateScreenWidth(40),
-      width: getProportionateScreenWidth(40),
-      decoration: BoxDecoration(
-        // color: product.colors[0],
-        shape: BoxShape.circle,
-        border:
-            Border.all(color: isSelected ? kPrimaryColor : Colors.transparent),
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
-      ),
     );
   }
 }
